34c85ca8364701a3ded15540280ab2b2833ea734
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2026-03-30 01:08:23 +0530

    feat: implement updateTask method with error handling for completed tasks

--- src/modules/v1/tasks/controller.ts
+++ src/modules/v1/tasks/controller.ts
@@ -1,8 +1,8 @@
 // internal-imports
-import { prismaClient, SuccessResponse } from '@/core/index.js';
+import { ErrorResponse, prismaClient, SuccessResponse } from '@/core/index.js';
 
 // type-imports
-import type { ISuccessResponse } from '@/core/index.js';
+import type { IErrorResponse, ISuccessResponse } from '@/core/index.js';
 import type { Request, Response } from 'express';
 
 // controller for module
@@ -45,7 +45,51 @@ export const controller = {
   },
 
   // @controller PATCH /:id
-  updateTask: (request: Request, response: Response) => {},
+  updateTask: async (
+    request: Request,
+    response: Response<ISuccessResponse<object> | IErrorResponse<null>>
+  ) => {
+    // create a object to hold the fields to be updated
+    const taskUpdationObject: { title?: string; description?: string; isCompleted?: boolean } = {};
+
+    // if the request body has taskTitle, add it to data object
+    if (request.body.taskTitle) taskUpdationObject.title = request.body.taskTitle;
+
+    // if the request body has taskDescription, add it to data object
+    if (request.body.taskDescription) taskUpdationObject.description = request.body.taskDescription;
+
+    // if the request body has isTaskCompleted
+    if (request.body.isTaskCompleted !== undefined) {
+      // if the task is completed and the request is marking it completed again, send error response
+      if (request.task.isCompleted && request.body.isTaskCompleted)
+        return response.status(400).json(
+          new ErrorResponse<null>({
+            message: 'Task is already marked as completed',
+            code: 'TASK_ALREADY_COMPLETED',
+            issues: null,
+          })
+        );
+
+      // add it to data object
+      taskUpdationObject.isCompleted = request.body.isTaskCompleted;
+    }
+
+    // update the task in db
+    const updatedTask = await prismaClient.task.update({
+      where: {
+        id: request.params.id as string,
+      },
+      data: taskUpdationObject,
+    });
+
+    // send success response
+    return response.status(200).json(
+      new SuccessResponse<object>({
+        message: 'Task updated successfully',
+        data: updatedTask,
+      })
+    );
+  },
 
   // @controller DELETE /:id
   deleteTask: (_request: Request, response: Response) => {},
