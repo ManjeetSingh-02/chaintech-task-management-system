2b0f0e573e1902f06863bc8763651a0116c04af6
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2026-03-29 23:31:43 +0530

    add validation schemas for task operations using Zod

---
+++ src/modules/v1/tasks/zod.ts
@@ -0,0 +1,50 @@
+// external-imports
+import z from 'zod';
+
+// schema for taskTitle
+const taskTitleSchema = z
+  .string()
+  .trim()
+  .nonempty({ error: 'taskTitle is required' })
+  .min(3, { error: 'taskTitle must be at least 3 characters long' })
+  .max(30, { error: 'taskTitle must be at most 30 characters long' });
+
+// schema for taskDescription
+const taskDescriptionSchema = z
+  .string()
+  .trim()
+  .nonempty({ error: 'taskDescription is required' })
+  .min(10, { error: 'taskDescription must be at least 10 characters long' })
+  .max(50, { error: 'taskDescription must be at most 50 characters long' });
+
+// schema for taskId
+const taskIdSchema = z.object({
+  id: z.uuid({ error: 'Task ID is required' }),
+});
+
+// schema for createTask
+export const createTaskSchema = z.object({
+  body: z.object({
+    taskTitle: taskTitleSchema,
+    taskDescription: taskDescriptionSchema,
+  }),
+});
+
+// schema for updateTask
+export const updateTaskSchema = z.object({
+  params: taskIdSchema,
+  body: z
+    .object({
+      taskTitle: taskTitleSchema.optional(),
+      taskDescription: taskDescriptionSchema.optional(),
+      isTaskCompleted: z.boolean().optional(),
+    })
+    .refine(data => Object.keys(data).length > 0, {
+      message: 'At least one field must be provided for update',
+    }),
+});
+
+// schema for deleteTask
+export const deleteTaskSchema = z.object({
+  params: taskIdSchema,
+});
