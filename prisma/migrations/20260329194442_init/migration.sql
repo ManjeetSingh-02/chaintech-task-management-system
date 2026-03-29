bff171391940842e8de92d5fc9399f70c138ca70
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2026-03-30 01:15:06 +0530

    feat: update description field to be optional

--- prisma/migrations/20260329194442_init/migration.sql
+++ prisma/migrations/20260329194442_init/migration.sql
@@ -2,7 +2,7 @@
 CREATE TABLE "Task" (
     "id" TEXT NOT NULL,
     "title" TEXT NOT NULL,
-    "description" TEXT NOT NULL,
+    "description" TEXT,
     "isCompleted" BOOLEAN NOT NULL DEFAULT false,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
