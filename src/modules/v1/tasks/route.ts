32d334bff70ac0fa2ce10c8e3f10df175a9dc798
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2026-03-29 23:31:51 +0530

    add validation and async handling for task routes

--- src/modules/v1/tasks/route.ts
+++ src/modules/v1/tasks/route.ts
@@ -1,5 +1,7 @@
 // internal-imports
+import { asyncHandler, validateZodSchema } from '@/core/index.js';
 import { controller } from './controller.js';
+import { createTaskSchema, deleteTaskSchema, updateTaskSchema } from './zod.js';
 
 // external-imports
 import { Router } from 'express';
@@ -8,13 +10,13 @@ import { Router } from 'express';
 export const router = Router();
 
 // @route POST /
-router.get('/', controller.createTask);
+router.get('/', validateZodSchema(createTaskSchema), asyncHandler(controller.createTask));
 
 // @route GET /
-router.get('/', controller.getAllTasks);
+router.get('/', asyncHandler(controller.getAllTasks));
 
 // @route PATCH /:id
-router.get('/:id', controller.updateTask);
+router.get('/:id', validateZodSchema(updateTaskSchema), asyncHandler(controller.updateTask));
 
 // @route DELETE /:id
-router.delete('/:id', controller.deleteTask);
+router.delete('/:id', validateZodSchema(deleteTaskSchema), asyncHandler(controller.deleteTask));
