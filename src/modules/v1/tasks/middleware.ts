d0a884e7a35e10eec429b191b212385e1e8e18d1
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2026-03-30 00:22:26 +0530

    feat: add middleware to check if task exists in database

---
+++ src/modules/v1/tasks/middleware.ts
@@ -0,0 +1,36 @@
+// internal-imports
+import { ErrorResponse, prismaClient } from '@/core/index.js';
+
+// type-imports
+import type { IErrorResponse } from '@/core/index.js';
+import type { Request, Response, NextFunction } from 'express';
+
+// middleware to check if task exists in database
+export const doesTaskExist = async (
+  request: Request,
+  response: Response<IErrorResponse<null>>,
+  nextFunction: NextFunction
+) => {
+  // fetch task from db
+  const existingTask = await prismaClient.task.findUnique({
+    where: {
+      id: request.params.id as string,
+    },
+  });
+
+  // if task does not exist, send error response
+  if (!existingTask)
+    return response.status(404).json(
+      new ErrorResponse<null>({
+        message: 'Task with given id does not exist',
+        code: 'TASK_NOT_FOUND',
+        issues: null,
+      })
+    );
+
+  // attach task to request object for use in controller
+  request.task = existingTask;
+
+  // forward request to next middleware or controller
+  nextFunction();
+};
