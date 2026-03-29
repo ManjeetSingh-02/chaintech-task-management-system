d6330eac933afa4098a4a3e33675823e894bba32
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2026-03-29 18:19:59 +0530

    add prismaClient export

--- src/core/index.ts
+++ src/core/index.ts
@@ -3,6 +3,9 @@ export { APP_CONFIG } from './config/constants.js';
 export { corsConfig } from './config/cors.js';
 export { env } from './config/env.js';
 
+// database
+export { prismaClient } from './database/prisma.js';
+
 // loader
 export { default as loadModules } from './loader/modules.js';
 
